5d7ef4482422b692e865d6f8defc20e809b7011c
Bidhya Sangroula <sangroba@mail.uc.edu>
2025-03-27 11:47:33 -0400

    Add project files.

---
+++ InClass_2025_03_27/Program.cs
@@ -0,0 +1,23 @@
+﻿//Bidhya Sangroula
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass_2025_03_27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Bidhya Sangroula");
+            Spring mySpring = new Spring();
+            mySpring.Place = "Nepal";
+            Console.WriteLine("Places to visit for spring: " + mySpring.Place);
+
+
+        }
+    }
+}
---
+++ InClass_2025_03_27/Spring.cs
@@ -0,0 +1,17 @@
+﻿//Bidhya Sangroula
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass_2025_03_27
+{
+    internal class Spring
+    {
+        
+        private string _place;
+        public string Place { get { return _place; } set { _place = value; } }
+
+    }
+}
